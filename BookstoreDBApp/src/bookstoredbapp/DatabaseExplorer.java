414322d83cbf45e91650f926e04a4fcb60048edd
Gerardo Gonzalez <Pawn0@users.noreply.github.com>
2019-04-20 13:57:16 -0400

    implemented the basics for doing queries that change the DB

--- BookstoreDBApp/src/bookstoredbapp/DatabaseExplorer.java
+++ BookstoreDBApp/src/bookstoredbapp/DatabaseExplorer.java
@@ -18,18 +18,14 @@ import java.util.ArrayList;
 public class DatabaseExplorer {
 
     //allows exploring the database, 
-    public void exploreDB(String query, int typeOfQuery) {
+    private void exploreDB(String query, int typeOfQuery) {
         DBConnector dbConnector = new DBConnector("spring19group2", "jZXg7V3p3VkD");
         ResultSet result = dbConnector.query(query);
         
         //query type 0 will find subscriptions via customer name
-        //type 1 will find
+        //type 1 will find customers via telephoneNumber
         //type 2 will find
-        switch(typeOfQuery){
-            case 0:
-                printResultSetForCustomerSubscriptions(result);
-                break;
-        }
+        printResultSet(result, typeOfQuery);
         
         dbConnector.close();
     }
@@ -39,17 +35,50 @@ public class DatabaseExplorer {
         String SQL = "SELECT series_title FROM subscription WHERE customer_name = '" + customerName + "'";
         exploreDB(SQL, 0);
     }
+    
+    public void findCustomersViaTelephoneNumber(String telephoneNumber){
+        String SQL = "SELECT customer_name FROM customers WHERE phone_number = '" + telephoneNumber + "'";
+        exploreDB(SQL, 1);
+    }
+    
+    public void comicBooksBySeriesName(String series_name){
+    
+    }
+    
+    public void comicBooksBuDateTheyComeOut(String date){
+        
+    }
+    public void comicBooksByPublisher(String publisher){
+        
+    }
+    public void customersByComicBookSeriesSubscribed(String series){
+        
+    }
+    
+    public void comicBooksOrderOnAGivenDateAndHowMany(String date){
+        
+    }
 
     //prints out the result set for all the customer subscriptions
     //essentially its saying result set get the value from colum series title
     //add it to display data, and print it out on the console
-    public void printResultSetForCustomerSubscriptions(ResultSet rs) {
+    private void printResultSet(ResultSet rs, int type) {
         displayData.clear();
         try {
             while (rs.next()) {
-                String series_title = rs.getString("series_title");
-                displayData.add(series_title);
-                System.out.println(series_title);
+                
+                switch(type){
+                    case 0:
+                        String series_title = rs.getString("series_title");
+                        displayData.add(series_title);
+                        System.out.println(series_title);
+                        break;
+                    case 1:
+                        String customer_name = rs.getString("customer_name");
+                        displayData.add(customer_name);
+                        System.out.println(customer_name);
+                        
+                }
             }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
--- BookstoreDBApp/src/bookstoredbapp/DatabaseUpdater.java
+++ BookstoreDBApp/src/bookstoredbapp/DatabaseUpdater.java
@@ -1,14 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bookstoredbapp;
+
+import java.sql.ResultSet;
 
 /**
  *
- * @author user
+ * @author Team2
  */
 public class DatabaseUpdater {
-    
+
+    public void UpdateDBDB(String query, int typeOfQuery) {
+        DBConnector dbConnector = new DBConnector("spring19group2", "jZXg7V3p3VkD");
+        ResultSet result = dbConnector.query(query);
+
+        //query type 0 will find subscriptions via customer name
+        //type 1 will find customers via telephoneNumber
+        //type 2 will find
+        executeChange(result, typeOfQuery);
+
+        dbConnector.close();
+    }
+
+    private void executeChange(ResultSet resultSet, int changeType) {
+    }
+
+    public void addNewCustomer() {
+    }
+
+    public void addNewSubscription(String customer) {
+    }
+
+    public void updateCustomer() {
+    }
+
+    public void changeDateComicBookIsReleased() {
+    }
+
+    public void deleteCustomer() {
+        deleteSubscriptionForCustomer();
+    }
+
+    public void deleteSubscriptionForCustomer() {
+    }
 }
