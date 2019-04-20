1875b20d73e08960f125aea5ed3c3786e3505cce
Gerardo Gonzalez <12078105+YellowRattlesnake@users.noreply.github.com>
2019-04-20 14:21:14 -0400

    changes to allow updating DB

--- BookstoreDBApp/src/bookstoredbapp/DatabaseUpdater.java
+++ BookstoreDBApp/src/bookstoredbapp/DatabaseUpdater.java
@@ -8,9 +8,9 @@ import java.sql.ResultSet;
  */
 public class DatabaseUpdater {
 
-    public void UpdateDBDB(String query, int typeOfQuery) {
+    public void UpdateDB(String query, int typeOfQuery) {
         DBConnector dbConnector = new DBConnector("spring19group2", "jZXg7V3p3VkD");
-        ResultSet result = dbConnector.query(query);
+        dbConnector.changingQuery(query);
 
         //query type 0 will find subscriptions via customer name
         //type 1 will find customers via telephoneNumber
