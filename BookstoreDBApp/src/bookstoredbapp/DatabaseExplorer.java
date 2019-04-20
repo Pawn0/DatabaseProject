296dc11d38e9e3537b9cbd7790c7465a03266f91
Gerardo Gonzalez <Pawn0@users.noreply.github.com>
2019-04-20 14:02:45 -0400

    changes

--- BookstoreDBApp/src/bookstoredbapp/DatabaseExplorer.java
+++ BookstoreDBApp/src/bookstoredbapp/DatabaseExplorer.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bookstoredbapp;
 
 import java.sql.ResultSet;
@@ -13,7 +8,6 @@ import java.util.ArrayList;
  *
  * @author Team2
  */
-
 //this class is used to read from the DB
 public class DatabaseExplorer {
 
@@ -21,12 +15,12 @@ public class DatabaseExplorer {
     private void exploreDB(String query, int typeOfQuery) {
         DBConnector dbConnector = new DBConnector("spring19group2", "jZXg7V3p3VkD");
         ResultSet result = dbConnector.query(query);
-        
+
         //query type 0 will find subscriptions via customer name
         //type 1 will find customers via telephoneNumber
         //type 2 will find
         printResultSet(result, typeOfQuery);
-        
+
         dbConnector.close();
     }
 
@@ -35,28 +29,30 @@ public class DatabaseExplorer {
         String SQL = "SELECT series_title FROM subscription WHERE customer_name = '" + customerName + "'";
         exploreDB(SQL, 0);
     }
-    
-    public void findCustomersViaTelephoneNumber(String telephoneNumber){
+
+    public void findCustomersViaTelephoneNumber(String telephoneNumber) {
         String SQL = "SELECT customer_name FROM customers WHERE phone_number = '" + telephoneNumber + "'";
         exploreDB(SQL, 1);
     }
-    
-    public void comicBooksBySeriesName(String series_name){
-    
+
+    public void comicBooksBySeriesName(String series_name) {
+
     }
-    
-    public void comicBooksBuDateTheyComeOut(String date){
-        
+
+    public void comicBooksBuDateTheyComeOut(String date) {
+
     }
-    public void comicBooksByPublisher(String publisher){
-        
+
+    public void comicBooksByPublisher(String publisher) {
+
     }
-    public void customersByComicBookSeriesSubscribed(String series){
-        
+
+    public void customersByComicBookSeriesSubscribed(String series) {
+
     }
-    
-    public void comicBooksOrderOnAGivenDateAndHowMany(String date){
-        
+
+    public void comicBooksOrderOnAGivenDateAndHowMany(String date) {
+
     }
 
     //prints out the result set for all the customer subscriptions
@@ -66,8 +62,8 @@ public class DatabaseExplorer {
         displayData.clear();
         try {
             while (rs.next()) {
-                
-                switch(type){
+
+                switch (type) {
                     case 0:
                         String series_title = rs.getString("series_title");
                         displayData.add(series_title);
@@ -77,14 +73,14 @@ public class DatabaseExplorer {
                         String customer_name = rs.getString("customer_name");
                         displayData.add(customer_name);
                         System.out.println(customer_name);
-                        
+
                 }
             }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
     }
-    
+
     //display data is what is used to get the information into one convenient location
     //we should call a function and then read the display data to get it to display somwhere
     ArrayList<String> displayData;
